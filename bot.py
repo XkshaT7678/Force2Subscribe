03440b41b1ece3f1bde3aac324d37a8d9a02c16e
Akki ThePro <73880884+Akshat7678@users.noreply.github.com>
2021-11-29 12:48:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
-#    This file is part of the ForveSub distribution (https://github.com/xditya/ForceSub).
-#    Copyright (c) 2021 Adiya
+#    This file is part of the ForveSub distribution (https://github.com/Akshat7678/Force2Subscribe).
+#    Copyright (c) 2021 Akshat7678
 #    
 #    This program is free software: you can redistribute it and/or modify  
 #    it under the terms of the GNU General Public License as published by  
@@ -10,7 +10,7 @@
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
 #    General Public License for more details.
 # 
-#    License can be found in < https://github.com/xditya/ForceSub/blob/main/License> .
+#    License can be found in < https://github.com/Akshat7678/Force2Subscribe/blob/main/License> .
 
 import logging
 import asyncio
@@ -45,7 +45,7 @@ except:
 
 if (apiid != None and apihash!= None and bottoken != None):
     try:
-        BotzHub = TelegramClient('BotzHub', apiid, apihash).start(bot_token=bottoken)
+        Arjvps = TelegramClient('Arjvps', apiid, apihash).start(bot_token=bottoken)
     except Exception as e:
         print(f"ERROR!\n{str(e)}")
         print("Bot is quiting...")
@@ -61,14 +61,14 @@ channel = xchannel.replace("@", "")
 async def get_user_join(id):
     ok = True
     try:
-        await BotzHub(GetParticipantRequest(channel=channel, participant=id))
+        await Arjvps(GetParticipantRequest(channel=channel, participant=id))
         ok = True
     except UserNotParticipantError:
         ok = False
     return ok
 
 
-@BotzHub.on(events.ChatAction())
+@Arjvps.on(events.ChatAction())
 async def _(event):
     if on_join is False:
         return
@@ -97,42 +97,42 @@ async def _(event):
         else:
             msg = welcome_not_joined.format(mention=mention, title=title, fullname=fullname, username=username, name=name, last=last, channel=f"@{channel}")
             butt = [Button.url("Channel", url=f"https://t.me/{channel}"), Button.inline("UnMute Me", data=f"unmute_{user.id}")]
-            await BotzHub.edit_permissions(event.chat.id, user.id, until_date=None, send_messages=False)
+            await Arjvps.edit_permissions(event.chat.id, user.id, until_date=None, send_messages=False)
         
         await event.reply(msg, buttons=butt)
 
 
-@BotzHub.on(events.NewMessage(incoming=True))
+@Arjvps.on(events.NewMessage(incoming=True))
 async def mute_on_msg(event):
     if event.is_private:
         return
     if on_new_msg is False:
         return
     x = await get_user_join(event.sender_id)
-    temp = await BotzHub(GetFullUserRequest(event.sender_id))
+    temp = await Arjvps(GetFullUserRequest(event.sender_id))
     if x is False:
         if temp.user.bot:
             return
         nm = temp.user.first_name
         try:
-            await BotzHub.edit_permissions(event.chat.id, event.sender_id, until_date=None, send_messages=False)
+            await Arjvps.edit_permissions(event.chat.id, event.sender_id, until_date=None, send_messages=False)
         except Exception as e:
             print(str(e))
             return
-        await event.reply(f"Hey {nm}, seems like you haven't joined our channel. Please join @{channel} and then press the button below to unmute yourself!", buttons=[[Button.url("Channel", url=f"https://t.me/{channel}")], [Button.inline("UnMute Me", data=f"unmute_{event.sender_id}")]])
+        await event.reply(f"Hey {nm}, seems like you haven't joined our channel. Please join @{channel} and then press the button below to unmute yourself!", buttons=[[Button.url("Channel", url=f"https://t.me/{channel}")], [Button.inline("Unmute Me", data=f"unmute_{event.sender_id}")]])
 
 
-@BotzHub.on(events.callbackquery.CallbackQuery(data=re.compile(b"unmute_(.*)")))
+@Arjvps.on(events.callbackquery.CallbackQuery(data=re.compile(b"unmute_(.*)")))
 async def _(event):
     uid = int(event.data_match.group(1).decode("UTF-8"))
     if uid == event.sender_id:
         x = await get_user_join(uid)
-        nm = (await BotzHub(GetFullUserRequest(uid))).user.first_name
+        nm = (await Arjvps(GetFullUserRequest(uid))).user.first_name
         if x is False:
             await event.answer(f"You haven't joined @{channel} yet!", cache_time=0, alert=True)
         elif x is True:
             try:
-                await BotzHub.edit_permissions(event.chat.id, uid, until_date=None, send_messages=True)
+                await Arjvps.edit_permissions(event.chat.id, uid, until_date=None, send_messages=True)
             except Exception as e:
                 print(str(e))
                 return
@@ -142,10 +142,10 @@ async def _(event):
     else:
         await event.answer("You are an old member and can speak freely! This isn't for you!", cache_time=0, alert=True)
 
-@BotzHub.on(events.NewMessage(pattern="/start"))
+@Arjvps.on(events.NewMessage(pattern="/start"))
 async def strt(event):
-    await event.reply(f"Hi. I'm a force subscribe bot made specially for @{channel}!\n\nCheckout @BotzHub :)", buttons=[Button.url("Channel", url=f"https://t.me/{channel}"), Button.url("Repository", url="https://github.com/xditya/ForceSub")])
+    await event.reply(f"Hi. I'm a force subscribe bot made specially for @{channel}!\n\nCheckout @TheArjvps for Updates..", buttons=[Button.url("Channel", url=f"https://t.me/{channel}"), Button.url("Repository", url="https://github.com/Akshat7678/Force2Subscribe")])
 
     
-print("ForceSub Bot has started.\nDo visit @BotzHub!")
-BotzHub.run_until_disconnected()
+print("ForceSub Bot has started.\nDo visit @TheArjvps!")
+Arjvps.run_until_disconnected()
